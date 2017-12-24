0183ecea468564f7c63afb8fe3a4e89c6292bf96
Joey Marianer <roie.marianer@gmail.com>
2017-12-24 15:43:06 -0800

    Render using Chrome headless; format titles.

--- renderall.sh
+++ renderall.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 for i in *.html ; do
-  phantomjs render.js $i ${i/\.html/.pdf}
+  # Google Chrome on MacOS
+  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --print-to-pdf $i
+  mv output.pdf ${i/\.html/.pdf}
 done
